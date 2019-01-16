be8d4fbed6922e312d0c4e5c12a0ee5b580944f9
Roushi Lin <fntsrlike@gmail.com>
2019-01-16 16:01:39 +0800

    Update last updated date

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@
           <footer class="text-center text-muted py-5">
             <a class="text-reset text-decoration-none" href="https://github.com/fntsrlike/fntsr.tw-2019" target="_blank" title="Github Repositroy">
               <time datetime="2019-01-14">
-                <small>最後更新：2019/01/14</small>
+                <small>最後更新：2019/01/15</small>
               </time>
             </a>
           </footer>
